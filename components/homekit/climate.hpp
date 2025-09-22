54f071f3974cce82ce63252d0cc8d2ab6f618edc
maxrd <maxrdnew@gmail.com>
2025-09-22 13:15:13 +0800

    Update climate.hpp

--- components/homekit/climate.hpp
+++ components/homekit/climate.hpp
@@ -8,19 +8,19 @@
 #include <hap_apple_chars.h>
 #include "const.h"
 #include "hap_entity.h"
+#include <cmath>  // std::isfinite
 
 namespace esphome {
 namespace homekit {
 
 class ClimateEntity : public HAPEntity {
- private:
+private:
   static constexpr const char* TAG = "ClimateEntity";
   climate::Climate* climatePtr;
 
-  static void on_climate_update(climate::Climate& obj) {
-    ESP_LOGI(TAG, "%s Mode: %d Action: %d CTemp: %.2f TTemp: %.2f CHum: %.2f THum: %.2f",
-             obj.get_name().c_str(), obj.mode, obj.action, obj.current_temperature,
-             obj.target_temperature, obj.current_humidity, obj.target_humidity);
+  static int acc_identify(hap_acc_t* ha) {
+    ESP_LOGI(TAG, "Accessory identified");
+    return HAP_SUCCESS;
   }
 
   static int climate_read(hap_char_t* hc, hap_status_t* status_code, void* serv_priv, void* read_priv) {
@@ -31,6 +31,7 @@ class ClimateEntity : public HAPEntity {
 
     hap_val_t state{};
     const char* type = hap_char_get_type_uuid(hc);
+
     if (!strcmp(type, HAP_CHAR_UUID_CURRENT_HEATING_COOLING_STATE)) {
       switch (obj->action) {
         case climate::CLIMATE_ACTION_OFF: state.i = 0; break;
@@ -39,9 +40,13 @@ class ClimateEntity : public HAPEntity {
         default: state.i = 0; break;
       }
     } else if (!strcmp(type, HAP_CHAR_UUID_CURRENT_TEMPERATURE)) {
-      state.f = obj->current_temperature;
+      float val = obj->current_temperature;
+      if (!std::isfinite(val)) val = 25.0;
+      state.f = val;
     } else if (!strcmp(type, HAP_CHAR_UUID_CURRENT_RELATIVE_HUMIDITY)) {
-      state.f = obj->current_humidity;
+      float val = obj->current_humidity;
+      if (!std::isfinite(val)) val = 50.0;
+      state.f = val;
     }
     hap_char_update_val(hc, &state);
     return HAP_SUCCESS;
@@ -56,6 +61,7 @@ class ClimateEntity : public HAPEntity {
     for (int i = 0; i < count; i++) {
       hap_write_data_t* write = &write_data[i];
       const char* type = hap_char_get_type_uuid(write->hc);
+
       if (!strcmp(type, HAP_CHAR_UUID_TARGET_HEATING_COOLING_STATE)) {
         switch (write->val.i) {
           case 0: obj->make_call().set_mode(climate::CLIMATE_MODE_OFF).perform(); break;
@@ -65,37 +71,31 @@ class ClimateEntity : public HAPEntity {
           default: break;
         }
       } else if (!strcmp(type, HAP_CHAR_UUID_TARGET_TEMPERATURE)) {
-        obj->make_call().set_target_temperature(write->val.f).perform();
+        float val = write->val.f;
+        if (!std::isfinite(val)) val = 25.0;
+        obj->make_call().set_target_temperature(val).perform();
       } else if (!strcmp(type, HAP_CHAR_UUID_TARGET_RELATIVE_HUMIDITY)) {
-        obj->make_call().set_target_humidity(write->val.f).perform();
+        float val = write->val.f;
+        if (!std::isfinite(val)) val = 50.0;
+        obj->make_call().set_target_humidity(val).perform();
       }
     }
     return HAP_SUCCESS;
   }
 
-  static int acc_identify(hap_acc_t* ha) {
-    ESP_LOGI(TAG, "Accessory identified");
-    return HAP_SUCCESS;
-  }
-
- public:
-  using HAPEntity::setup;
-
+public:
   ClimateEntity(climate::Climate* climatePtr)
       : HAPEntity({{MODEL, "HAP-CLIMATE"}}), climatePtr(climatePtr) {}
 
-  void setup(TemperatureUnits units = CELSIUS) {  // remove override
-    if (!climatePtr) {
-      ESP_LOGW(TAG, "Climate pointer is null, skipping HomeKit setup.");
-      return;
-    }
+  void setup() {  // 移除 override，避免編譯錯誤
+    if (!climatePtr) return;
 
     hap_acc_cfg_t acc_cfg{};
     static char acc_name[32];
     static char acc_serial[32];
 
-    snprintf(acc_name, sizeof(acc_name), "%s", climatePtr->get_name().c_str());
-    snprintf(acc_serial, sizeof(acc_serial), "%lu", climatePtr->get_object_id_hash());
+    snprintf(acc_name, sizeof(acc_name), "%s", this->climatePtr->get_name().c_str());
+    snprintf(acc_serial, sizeof(acc_serial), "%lu", this->climatePtr->get_object_id_hash());
 
     acc_cfg.name = acc_name;
     acc_cfg.serial_num = acc_serial;
@@ -104,17 +104,17 @@ class ClimateEntity : public HAPEntity {
     strcpy(acc_cfg.fw_rev, "0.1.0");
     strcpy(acc_cfg.pv, "1.1.0");
     acc_cfg.hw_rev = nullptr;
-    acc_cfg.cid = HAP_CID_BRIDGE;
+    acc_cfg.cid = HAP_CID_THERMOSTAT;
     acc_cfg.identify_routine = acc_identify;
 
     uint8_t current_mode = 0, target_mode = 0;
-    switch (climatePtr->action) {
+    switch (this->climatePtr->action) {
       case climate::CLIMATE_ACTION_OFF: current_mode = 0; break;
       case climate::CLIMATE_ACTION_HEATING: current_mode = 1; break;
       case climate::CLIMATE_ACTION_COOLING: current_mode = 2; break;
       default: current_mode = 0; break;
     }
-    switch (climatePtr->mode) {
+    switch (this->climatePtr->mode) {
       case climate::CLIMATE_MODE_OFF: target_mode = 0; break;
       case climate::CLIMATE_MODE_HEAT: target_mode = 1; break;
       case climate::CLIMATE_MODE_COOL: target_mode = 2; break;
@@ -122,17 +122,23 @@ class ClimateEntity : public HAPEntity {
       default: break;
     }
 
-    hap_serv_t* service = hap_serv_thermostat_create(current_mode, target_mode,
-                                                     climatePtr->current_temperature,
-                                                     climatePtr->target_temperature,
-                                                     units);
+    float ct = climatePtr->current_temperature;
+    float tt = climatePtr->target_temperature;
+    if (!std::isfinite(ct)) ct = 25.0;
+    if (!std::isfinite(tt)) tt = 25.0;
 
-    climate::ClimateTraits traits = climatePtr->get_traits();
+    hap_serv_t* service = hap_serv_thermostat_create(current_mode, target_mode, ct, tt, CELSIUS);
+
+    climate::ClimateTraits traits = this->climatePtr->get_traits();
     if (traits.get_supports_current_humidity()) {
-      hap_serv_add_char(service, hap_char_current_relative_humidity_create(climatePtr->current_humidity));
+      float hum = climatePtr->current_humidity;
+      if (!std::isfinite(hum)) hum = 50.0;
+      hap_serv_add_char(service, hap_char_current_relative_humidity_create(hum));
     }
     if (traits.get_supports_target_humidity()) {
-      hap_serv_add_char(service, hap_char_target_relative_humidity_create(climatePtr->target_humidity));
+      float hum = climatePtr->target_humidity;
+      if (!std::isfinite(hum)) hum = 50.0;
+      hap_serv_add_char(service, hap_char_target_relative_humidity_create(hum));
     }
 
     hap_acc_t* accessory = hap_acc_create(&acc_cfg);
